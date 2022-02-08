ad2c8a9000efe2a5dc53624d103e6c217091e912
nicoripkip <nicovanommen.nvo@gmail.com>
2022-02-08 15:53:29 +0100

    Java omgeving opgezet

---
+++ src/main/App.java
@@ -0,0 +1,11 @@
+package main;
+
+
+/**
+ * @author Niko van Ommen
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
